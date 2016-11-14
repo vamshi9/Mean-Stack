e8a80ba97a22ec596e8c38c6503f5d66539270ef
vamshi <vamsisai999@gmail.com>
2016-11-15 02:03:46 +0530

    Revert "MVC"
    This reverts commit 52a0d1d452cb403c51997c37260988cebf23a474.

--- app_server/controllers/location.js
+++ app_server/controllers/location.js
@@ -1,31 +1,7 @@
 module.exports.homePage = function(req, res) {
-        res.render('location-list', {
-           title: 'vb9',
-           pageHeader : {
-              title : 'VB9',
-              strapline : 'Find nearby Foodie Restaurants!'
-           },
-           locations : [{
-             name : 'Starbucks',
-             address : 'Inorbit Mall, Inorbit Mall Road',
-             rating :5,
-             facilities : ['Hot Drinks','Cold Drinks','Mixed Drinks'],
-             distance : '2000m'
-           },{
-             name : 'Coffee Day',
-             address : 'Urdu University Road,Telecom Nagar, Gachibowli',
-             rating :4,
-             facilities : ['Hot Drinks','Cold Drinks','Mixed Drinks'],
-             distance : '300m'
-           },{
-             name : 'KFC',
-             address : 'Indira Nagr, Gachibowli',
-             rating :5,
-             facilities : ['Burgers' ,'Veg', 'Non-Veg','Soft Drinks'],
-             distance : '300m'
-           }]
-          });
+        res.render('location-list', { title: 'Home' });
 };
+
 module.exports.homeList = function(req, res) {
         res.render('location-list', { title: 'Home' });
 };
