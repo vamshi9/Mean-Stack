03b5e1c0a5c91938027f8ab8595fc0415a3a0a99
vamshi <vamsisai999@gmail.com>
2016-11-17 00:29:15 +0530

    location-info

--- app_server/controllers/location.js
+++ app_server/controllers/location.js
@@ -27,14 +27,47 @@ module.exports.homePage = function(req, res) {
           });
 };
 
+module.exports.locationDetails = function(req,res){
+    res.render('location-info',{
+     title : 'Starbucks',
+     pageHeader : {
+       title : 'Starbucks'
+     },
+     location : {
+       name : 'Starbucks',
+       address : 'Inorbit Mall, Inorbit Mall Road',
+       rating : 5,
+       timings : {
+         title : 'Opening Hours',
+         openingHours : 'Monday to Saturday 10:30 AM - 10:00 PM'
+       },
+       menu : {
+         title : 'Menu Offered',
+         facilities : ['Hot Drinks','Food','Premium Wifi']
+       },
+       locationTitle : 'Location Map',
+       reviewTitle : 'Add Review',
+       customerReviews : 'Customer Reviews',
+     },
+     users : [{
+       name :'Vamshi Bachenboina',
+       comment : 'This is awesome place to try all the type of Coffeeeee! <3',
+       date : '27 July 2016',
+       rating : 5
+     },{
+       name : 'A G Praneeth',
+       comment : ' Well! Not satifised with money to taste! :/',
+       date : '27 July 2016',
+       rating : 4,
+     }]
+   });
+};
+
+
 module.exports.addReview = function(req,res){
   res.render('add-review',{title : 'Add Review'});
 };
 
 module.exports.homeList = function(req, res) {
         res.render('location-list', { title: 'Home' });
-};
-
-module.exports.locationDetails = function(req,res){
-  res.render('location-info',{ title : 'Location Details'});
 };
